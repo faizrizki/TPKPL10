9f59b38384653a0bac15f08c4bbcb1234101ef09
Muhammad Rizki <mrizki282930@gmail.com>
2024-04-29 03:01:35 +0700

    menambahkan API mahasiswa

---
+++ TPMOD10/Controllers/mahasiswacontroller.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TPMOD10;
+
+namespace TPMOD10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> listMahasiswa = new List<Mahasiswa>
+    {
+        new Mahasiswa(" Muhammad Rizki", "1302220128"),
+        new Mahasiswa(" Salman Hamala Rizky", "1302220122"),
+        new Mahasiswa(" Afzaal Isnaufal", "1302220104"),
+        new Mahasiswa(" Athallah Muhammad Abdul Aziz", "1302223125")
+    };
+        [HttpGet]
+
+
+
+
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return listMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return listMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mahasiswa)
+        {
+            listMahasiswa.Add(mahasiswa);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            listMahasiswa.RemoveAt(id);
+        }
+    }
+
+}
---
+++ TPMOD10/mahasiswa.cs
@@ -0,0 +1,15 @@
+﻿namespace TPMOD10
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string NIM { get; set; }
+
+        public Mahasiswa(string Nama, string NIM)
+        {
+            this.Nama = Nama;
+            this.NIM = NIM;
+        }
+    }
+
+}
